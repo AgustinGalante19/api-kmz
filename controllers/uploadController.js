1573cf894c149d452b8968664a87f85974dc81ab
AgustinGalante19 <agustin.galante.19@outlook.es>
2022-07-26 11:02:58 -0300

    crear funciones de tablas

---
+++ controllers/uploadController.js
@@ -0,0 +1,37 @@
+import parseKMZ from 'parse2-kmz'
+import fs from 'fs'
+
+export default (req, res) => {
+    const { filename } = req.file;
+
+    parseKMZ.toJson('./temp/' + filename)
+        .then((r) => {
+            const { features } = r;
+            let polygons = [];
+            let pointers = [];
+            let aux = [];
+            features.forEach((feature, i) => {
+                if (feature.geometry.type === 'Polygon') {
+                    feature.geometry.coordinates[0].map((item) => {
+                        aux.push({ lat: item[1], lng: item[0] })
+                    })
+                    polygons.push({
+                        coords: aux,
+                        name: feature.properties.name
+                    })
+                    aux = []
+                } else if (feature.geometry.type === 'Point') {
+                    pointers.push({
+                        coords: { lat: feature.geometry.coordinates[1], lng: feature.geometry.coordinates[0] },
+                        name: feature.properties.name
+                    })
+                }
+            });
+            fs.unlinkSync('./temp/' + filename);
+            res.json({ polygons, center: polygons[0].coords[0], pointers });
+        })
+        .catch((e) => {
+            console.error(e);
+            res.send('no ok').end();
+        });
+}
--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import cors from 'cors'
 import morgan from 'morgan'
 import path from 'path'
 import multer from './libs/multer.js'
-import parseKMZ from 'parse2-kmz'
-
-
+import uploadController from './controllers/uploadController.js'
 const app = express();
 
 
@@ -14,7 +12,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/uploads', express.static(path.resolve('uploads')));
+app.use('/temp', express.static(path.resolve('temp')));
 
 app.set("port", process.env.PORT || 5000);
 
@@ -22,19 +20,7 @@ app.get('/', (req, res) => {
     res.send('kmz api by agustin galante');
 })
 
-app.post('/upload', multer.single('archivo'), (req, res) => {
-    const { filename } = req.file;
-
-    parseKMZ.toJson('./uploads/' + filename)
-        .then((r) => {
-            const { coordinates } = r.features[0].geometry
-            res.json(coordinates).end();
-        })
-        .catch((e) => {
-            console.error(e);
-            res.send('no ok').end();
-        });
-});
+app.post('/upload', multer.single('archivo'), uploadController);
 
 app.listen(process.env.PORT || app.get("port"), () => {
     console.log('server on port 5000')
