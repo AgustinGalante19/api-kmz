6b68167716b9545f327ee7d023f2511978e7cdb4
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2022-09-14 08:47:54 -0300

    14.09.2022

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import path from 'path'
-import multer from './libs/multer.js'
-import uploadController from './controllers/uploadController.js'
-const app = express();
+import express from "express"
+import cors from "cors"
+import morgan from "morgan"
+import path from "path"
+import multer from "./libs/multer.js"
+import uploadController from "./controllers/uploadController.js"
+const app = express()
 
-
-app.use(express.json());
-app.use(cors());
-app.use(morgan("dev"));
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/temp', express.static(path.resolve('temp')));
+app.use("/temp", express.static(path.resolve("temp")))
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 5000)
 
-app.get('/', (req, res) => {
-    res.send('kmz api by agustin galante');
+app.get("/", (req, res) => {
+    res.send("kmz api by agustin galante")
 })
 
-app.post('/upload', multer.single('archivo'), uploadController);
+app.post("/put", (req, res) => {
+    res.json({
+        message: "ok",
+        result: 1,
+    })
+})
+
+app.post("/upload", multer.single("archivo"), uploadController)
 
 app.listen(process.env.PORT || app.get("port"), () => {
-    console.log('server on port 5000')
+    console.log("server on port 5000")
 })
